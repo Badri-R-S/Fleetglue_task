93f416fce7dfa716430b66a130f48d6e9e9653d1
Badri-R-S <badrinarayanan008@gmail.com>
2024-11-16 00:45:10 -0600

    REST API server

--- src/fg_task/fg_task/app.py
+++ src/fg_task/fg_task/app.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-data_store = None  # Global variable to store the mission data
+data_store = None  # Variable to store the task data published by the task publisher
 
+#POST endpoint definition. When a POST request is made to the /task endpoint, this function gets executed
 @app.route('/task', methods=['POST'])
-def post_mission():
+def post_task():
     global data_store
     data_store = request.json  # Store the JSON data from POST request
-    return jsonify({"status": "Mission data received"}), 200
+    return jsonify({"status": "Task received"}), 200
 
+#GET endpoint definition. Retrieves data from the endpoint
 @app.route('/task', methods=['GET'])
-def get_mission():
+def get_task():
     if data_store is not None:
         return jsonify(data_store), 200
     else:
-        return jsonify({"status": "No mission data available"}), 404
+        return jsonify({"status": "No tasks available"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
